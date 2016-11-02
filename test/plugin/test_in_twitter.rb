e4cfb4df1e4eae7cc3604e52607402c1c42886cd
Kenji Okimoto <okimoto@clear-code.com>
2016-11-02 14:02:07 +0900

    Indent

--- test/plugin/test_in_twitter.rb
+++ test/plugin/test_in_twitter.rb
@@ -21,46 +21,46 @@ class TwitterInputTest < Test::Unit::TestCase
   end
 
   sub_test_case "v0 syntax" do
-  def test_configure
-    assert_raise(Fluent::ConfigError) {
-      d = create_driver('', syntax: :v0)
-    }
-    d = create_driver %[
-      consumer_key        CONSUMER_KEY
-      consumer_secret     CONSUMER_SECRET
-      access_token        ACCESS_TOKEN
-      access_token_secret ACCESS_TOKEN_SECRET
-      tag                 input.twitter
-      timeline            tracking
-      keyword             ${hashtag}fluentd,fluentd lang:ja
-    ]
-    assert_equal 'CONSUMER_KEY', d.instance.consumer_key
-    assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
-    assert_equal 'ACCESS_TOKEN', d.instance.access_token
-    assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
-    assert_equal '#fluentd,fluentd lang:ja', d.instance.keyword
-  end
+    def test_configure
+      assert_raise(Fluent::ConfigError) {
+        d = create_driver('', syntax: :v0)
+      }
+      d = create_driver %[
+        consumer_key        CONSUMER_KEY
+        consumer_secret     CONSUMER_SECRET
+        access_token        ACCESS_TOKEN
+        access_token_secret ACCESS_TOKEN_SECRET
+        tag                 input.twitter
+        timeline            tracking
+        keyword             ${hashtag}fluentd,fluentd lang:ja
+      ]
+      assert_equal 'CONSUMER_KEY', d.instance.consumer_key
+      assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
+      assert_equal 'ACCESS_TOKEN', d.instance.access_token
+      assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
+      assert_equal '#fluentd,fluentd lang:ja', d.instance.keyword
+    end
   end
 
   sub_test_case "v1 syntax" do
-  def test_v1_multi_keyword
-    assert_raise(Fluent::ConfigError) {
-      d = create_driver('')
-    }
-    d = create_driver(%[
-      consumer_key        CONSUMER_KEY
-      consumer_secret     CONSUMER_SECRET
-      access_token        ACCESS_TOKEN
-      access_token_secret ACCESS_TOKEN_SECRET
-      tag                 input.twitter
-      timeline            tracking
-      keyword             'treasuredata,treasure data,#treasuredata,fluentd,#fluentd'
-    ])
-    assert_equal 'CONSUMER_KEY', d.instance.consumer_key
-    assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
-    assert_equal 'ACCESS_TOKEN', d.instance.access_token
-    assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
-    assert_equal 'treasuredata,treasure data,#treasuredata,fluentd,#fluentd', d.instance.keyword
-  end
+    def test_v1_multi_keyword
+      assert_raise(Fluent::ConfigError) {
+        d = create_driver('')
+      }
+      d = create_driver(%[
+        consumer_key        CONSUMER_KEY
+        consumer_secret     CONSUMER_SECRET
+        access_token        ACCESS_TOKEN
+        access_token_secret ACCESS_TOKEN_SECRET
+        tag                 input.twitter
+        timeline            tracking
+        keyword             'treasuredata,treasure data,#treasuredata,fluentd,#fluentd'
+      ])
+      assert_equal 'CONSUMER_KEY', d.instance.consumer_key
+      assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
+      assert_equal 'ACCESS_TOKEN', d.instance.access_token
+      assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
+      assert_equal 'treasuredata,treasure data,#treasuredata,fluentd,#fluentd', d.instance.keyword
+    end
   end
 end
