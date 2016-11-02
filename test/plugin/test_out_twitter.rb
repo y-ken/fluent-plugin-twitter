e0e46f726321122dc1cc5abf8aa7b3e81e70c8f2
Kenji Okimoto <okimoto@clear-code.com>
2016-11-02 14:05:54 +0900

    Indent

--- test/plugin/test_out_twitter.rb
+++ test/plugin/test_out_twitter.rb
@@ -19,33 +19,33 @@ class TwitterOutputTest < Test::Unit::TestCase
   end
 
   sub_test_case "configure" do
-  def test_configure
-    assert_raise(Fluent::ConfigError) {
-      d = create_driver('')
-    }
-    d = create_driver %[
-      consumer_key        CONSUMER_KEY
-      consumer_secret     CONSUMER_SECRET
-      access_token        ACCESS_TOKEN
-      access_token_secret ACCESS_TOKEN_SECRET
-    ]
-    assert_equal 'CONSUMER_KEY', d.instance.consumer_key
-    assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
-    assert_equal 'ACCESS_TOKEN', d.instance.access_token
-    assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
-  end
+    def test_configure
+      assert_raise(Fluent::ConfigError) {
+        d = create_driver('')
+      }
+      d = create_driver %[
+        consumer_key        CONSUMER_KEY
+        consumer_secret     CONSUMER_SECRET
+        access_token        ACCESS_TOKEN
+        access_token_secret ACCESS_TOKEN_SECRET
+      ]
+      assert_equal 'CONSUMER_KEY', d.instance.consumer_key
+      assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
+      assert_equal 'ACCESS_TOKEN', d.instance.access_token
+      assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
+    end
 
-  def test_configure_compatible
-    d = create_driver %[
-      consumer_key        CONSUMER_KEY
-      consumer_secret     CONSUMER_SECRET
-      oauth_token         ACCESS_TOKEN
-      oauth_token_secret  ACCESS_TOKEN_SECRET
-    ]
-    assert_equal 'CONSUMER_KEY', d.instance.consumer_key
-    assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
-    assert_equal 'ACCESS_TOKEN', d.instance.access_token
-    assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
-  end
+    def test_configure_compatible
+      d = create_driver %[
+        consumer_key        CONSUMER_KEY
+        consumer_secret     CONSUMER_SECRET
+        oauth_token         ACCESS_TOKEN
+        oauth_token_secret  ACCESS_TOKEN_SECRET
+      ]
+      assert_equal 'CONSUMER_KEY', d.instance.consumer_key
+      assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
+      assert_equal 'ACCESS_TOKEN', d.instance.access_token
+      assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
+    end
   end
 end
