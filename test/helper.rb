b460b7d5e3deb48f7fd0edabf8342f393315f41a
Kenji Okimoto <okimoto@clear-code.com>
2016-11-07 11:43:48 +0900

    Add load path properly

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 require 'test/unit'
 
-$LOAD_PATH.unshift(__dir__, '..', 'lib')
+$LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
 unless ENV.has_key?('VERBOSE')
