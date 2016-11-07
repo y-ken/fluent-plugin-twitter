0dab594a241cf42619b819f02f698b4f1b50aa16
Kenji Okimoto <okimoto@clear-code.com>
2016-11-07 15:48:36 +0900

    Remove outdated test

--- test/plugin/test_out_twitter.rb
+++ test/plugin/test_out_twitter.rb
@@ -37,18 +37,5 @@ class TwitterOutputTest < Test::Unit::TestCase
       assert_equal 'ACCESS_TOKEN', d.instance.access_token
       assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
     end
-
-    def test_configure_compatible
-      d = create_driver %[
-        consumer_key        CONSUMER_KEY
-        consumer_secret     CONSUMER_SECRET
-        oauth_token         ACCESS_TOKEN
-        oauth_token_secret  ACCESS_TOKEN_SECRET
-      ]
-      assert_equal 'CONSUMER_KEY', d.instance.consumer_key
-      assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
-      assert_equal 'ACCESS_TOKEN', d.instance.access_token
-      assert_equal 'ACCESS_TOKEN_SECRET', d.instance.access_token_secret
-    end
   end
 end
